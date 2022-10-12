75caa8705db917ab60294583fb24b3bf32f81f60
JonathanValleA <jvalle@jaumebalmes.net>
2022-10-12 16:50:30 +0200

    Conexion a la base de datos MySQl y productos

---
+++ UF1/Practica4/conection.php
@@ -0,0 +1,12 @@
+<?php 
+// Conexion a mi Base de datos
+        $db_host = "localhost";
+        $db_nombre = "products";
+        $db_usuario = "root";
+        $db_passwd = "";
+
+        $connexio = mysqli_connect($db_host, $db_usuario, $db_passwd, $db_nombre);
+        $consulta = "SELECT * FROM PRODUCTO";
+
+        $products = mysqli_query($connexio, $consulta);
+?>
