286fcec9c33f956644b31653fb1b39817dd6b562
JonathanValleA <113449282+JonathanValleA@users.noreply.github.com>
2022-10-20 22:04:52 +0200

    Branch final (#9)
    * Conexion a la base de datos MySQL y productos
    
    * Tabla Productos Botones Add, Edit, Delete
    
    * Access a la Bbdd amb PDO
    
    * Editar, añadir y eliminar productos y conexion a la Base de datos
    
    * Practicas
    
    * Añadir eliminar y editar producto y conexion a base de datos

--- UF1/Practica4/index.php
+++ UF1/Practica4/index.php
@@ -1,26 +1,7 @@
-
 <?php
-    try{
-            $connect_PDO = new PDO ("mysql:host=localhost; dbname=products", "root", "");
-            //Capturem errors de l’objecte Exception per a mostrar en el catch
-            $connect_PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Connexió correcte!";     
-    }catch(Exception $e){
-            die("Error: " . $e->GetMessage());
-    }finally{
-            $connect_PDO = null;
-    }
-//Fem la QUERY per obtindre tota la info de la taula
-$consulta = "SELECT * FROM PRODUCTO";
-$products = exec($consulta);
+    include 'conection.php';
 ?>
 <html>
-
-<?php 
-    include 'conection.php';
-    $sql = "SELECT * FROM PRODUCTO";
-?>
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -31,7 +12,6 @@ $products = exec($consulta);
 <table class="table">
     <thead>
     	<tr>
-	// 
           <th scope="col">#</th>
           <th scope="col">Id</th>
           <th scope="col">Name</th>
@@ -40,52 +20,13 @@ $products = exec($consulta);
         </tr>
     </thead>
     <tbody>
-
 	
-      <?php foreach ($products as $i => $product){ ?>
-        <tr>
-            <th scope="row"><?php echo $i +1 ?></th> <!-- augmentem el index i -->
-            <td><?php echo $producto['id'] ?></td> <!--Accedim a NumID -->
-            <td><?php echo $producto['Name'] ?></td> <!--Accedim a Name-->
-            <td><?php echo $producto['Descripcion'] ?></td> <!--Accedim a Description-->
-            <td><?php echo $producto['Price'] ?></td> <!--Accedim a Price -->
-            <td><a href="edit.php?id=<?php echo $producto['id']?>"><button type="button" class="btn btn-outline-primary">Edit</button></a></td>
-            <td><a href="delete.php?id=<?php echo $producto['id']?>"><button type="button" class="btn btn-outline-danger">Delete</button></a></td>
-        </tr>
-     <?php } ?>
-    </tbody>
-</table>
-<!-- SECCIÓ PER AFEGIR PRODUCTES --> 
-<div class="container p-4">
-		<div class="row">
-			<div class="col-md-4">
-				<div class="card card-body">
-				<!-- A través del mètode POST li enviem les dades del formulari a l'arxiu add_product.php -->
-					<form action="añadir_producto.php" method="POST"> 
-						<div class=form-group>
-							<input type="text" name="name" class="form-control" placeholder="Name" autofocus>
-						</div>
-						<div class=form-group>
-							<textarea name="description" rows="3" class="form-control" placeholder="Description"></textarea>
-						</div>
-						<div class=form-group>
-							<input type="text" name="price" class="form-control" placeholder="price">
-						</div>
-						<div class=form-group>
-							<input type="text" name="quantity" class="form-control" placeholder="Quantity">
-						</div>
-						<input type="submit" class="btn btn-success btn-block" name="add_product" value="+ Producte">
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
-</body>
-</html>
-
-
-        <!-- Ir añadiendo los productos en la tabla -->
-        <?php $resultado = mysqli_query($conexion, $sql);
+        <?php
+            include 'conection.php';
+            $sql = "SELECT * FROM producto";
+        ?>
+       <!-- Ir añadiendo los productos en la tabla -->
+       <?php $resultado = mysqli_query($conexion, $sql);
         $contador = 1;
             while($product=mysqli_fetch_assoc($resultado)) { ?>
 
@@ -99,6 +40,8 @@ $products = exec($consulta);
                 </tr>
          <?php } mysqli_free_result($resultado); ?>
          <div><a class="btn btn-success" href="añadir.php">Añadir</a></div>
+    </tbody>
 </table>
 </body>
+</html>
 
